44b512aa3e40a650849bc726c9a6628acf6ba2c9
Jorge Muñoz <167018435+Ser2k2@users.noreply.github.com>
2025-01-26 14:14:40 -0600

    Versión final juego Amigo Secreto

---
+++ app.js
@@ -0,0 +1,50 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    
+    let nombre = document.getElementById('amigo').value;
+  
+    if (nombre === '') {
+
+        alert('Por favor, inserte un nombre!')
+        
+    } else {
+
+        amigos.push(nombre);
+
+        let lista = '';    
+    
+        for (let i = 0; i < amigos.length; i++) {
+            
+            lista += '<li>' + amigos[i] + '</li>';
+        }  
+         
+                
+         document.getElementById('listaAmigos').innerHTML = lista;
+         document.getElementById('amigo').value = '';
+
+    }    
+}
+
+function sortearAmigo() {
+
+    let vacio = [];
+
+    if (amigos.length === 0) {
+
+        alert('Por favor, inserte un nombre!')
+
+    } else {
+
+        let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+    
+        document.getElementById('listaAmigos').innerHTML = `El amigo sorteado es: ${amigos[numeroAleatorio]}`;
+
+    }
+    
+    
+
+}
+
